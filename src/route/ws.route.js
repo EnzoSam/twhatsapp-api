e1a5e8d3cc19104fd8a92b34cb1dccfa0c6b7122
Enzo Samudio <enzosamudio@gmail.com>
2023-10-06 16:41:29 -0300

    ROUTE

--- src/route/ws.route.js
+++ src/route/ws.route.js
@@ -1,11 +1,10 @@
-
 var express = require('express');
 var WSController = require('../controller/wsController.controller');
 
 var router = express.Router();
 
-router.get("/", WSController.test); 
 router.get("/webhook", WSController.verify);  
 router.post("/webhook", WSController.processMessage);
+router.get("/", WSController.test); 
 
 module.exports = router;

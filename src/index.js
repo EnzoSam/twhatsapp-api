a7003c679abdee3b866f4d9b44ae8d28043133d7
Enzo Samudio <enzosamudio@gmail.com>
2023-10-06 10:38:27 -0300

    creacion de variables de entorno firevase

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 const dotenv = require('dotenv');
 const path = require('path');
-//const admin = require('firebase-admin');
-//var serviceAccount = require("../twhatsapp-a6696-firebase-adminsdk-g8ygn-3e9aaf585d.json");
+const admin = require('firebase-admin');
 
 dotenv.config({
     path: path.resolve(__dirname, process.env.NODE_ENV + '.env')
   });
 
-console.log(process.env.DATABASE_URL);
-/*
+const serviceAccount = {
+  type:  process.env.SERVICE_TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: JSON.parse(process.env.PRIVATE_KEY).private_key,
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_ORIVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN
+}
+
 admin.initializeApp(
   {
     credential: admin.credential.cert(serviceAccount),
     databaseURL: process.env.DATABASE_URL
   }
 )
-*/
+
 var app = require('./app');
 var port = process.env.PORT || 3999;
 

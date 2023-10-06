7bdbdd041d9e3f2c3d82c4d0589657f61189f13e
Enzo Samudio <enzosamudio@gmail.com>
2023-10-06 09:58:31 -0300

    revocacion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 const path = require('path');
-const admin = require('firebase-admin');
-var serviceAccount = require("../twhatsapp-a6696-firebase-adminsdk-g8ygn-3e9aaf585d.json");
+//const admin = require('firebase-admin');
+//var serviceAccount = require("../twhatsapp-a6696-firebase-adminsdk-g8ygn-3e9aaf585d.json");
 
 dotenv.config({
     path: path.resolve(__dirname, process.env.NODE_ENV + '.env')
